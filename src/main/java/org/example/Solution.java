cbe2e2eb640e986a972483f060836f6968dd8a02
Alvin <alvinngg9@gmail.com>
2024-08-07 18:09:18 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,16 +1,24 @@
 package org.example;
 
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Solution {
     public int[][] kClosest(int[][] points, int K) {
-        Arrays.sort(points, (a, b) -> {
-            return (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]);
-        });
+        //initialize the priority queue with the distance equation
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0] * a[0] + a[1] * a[1]));
+        //initialize the 2d array
         int[][] result = new int[K][2];
+
+        //add points to the priority queue
+        for (int[] point : points) {
+            pq.offer(point);
+        }
+
+        //remove points from the priority queue and add them to the result
         for (int i = 0; i < K; i++) {
-            result[i][0] = points[i][0];
-            result[i][1] = points[i][1];
+            result[i][0] = pq.peek()[0];
+            result[i][1] = pq.remove()[1];
         }
         return result;
     }
